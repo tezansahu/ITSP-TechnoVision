3c90a92a409ca135114d8ecb82123022dfc861b8
tusharag121099 <tusharag121099@gmail.com>
2018-06-17 16:56:47 +0530

    Bot control layout added

--- Bot_Controller/app/src/main/java/com/example/shaurya/bot_controller/A2_Remote.java
+++ Bot_Controller/app/src/main/java/com/example/shaurya/bot_controller/A2_Remote.java
@@ -1,7 +1,11 @@
 package com.example.shaurya.bot_controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class A2_Remote extends AppCompatActivity {
 
@@ -9,5 +13,64 @@ public class A2_Remote extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a2__remote);
+        ImageView up_image = (ImageView) findViewById(R.id.imageup);
+        ImageView left_image = (ImageView) findViewById(R.id.imageleft);
+        ImageView right_image = (ImageView) findViewById(R.id.imageright);
+        ImageView down_image = (ImageView) findViewById(R.id.imagedown);
+        ImageView servoleft_image = (ImageView) findViewById(R.id.servoleft);
+        ImageView servoright_image = (ImageView) findViewById(R.id.servoright);
+
+
+        up_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(A2_Remote.this, "YOu clicked up", Toast.LENGTH_SHORT).show();
+            }
+        });
+        left_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(A2_Remote.this, "YOu clicked left", Toast.LENGTH_SHORT).show();
+            }
+        });
+        right_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(A2_Remote.this, "YOu clicked right", Toast.LENGTH_SHORT).show();
+            }
+        });
+        down_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(A2_Remote.this, "YOu clicked down", Toast.LENGTH_SHORT).show();
+            }
+        });
+        servoleft_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(A2_Remote.this, "YOu clicked servoleft", Toast.LENGTH_SHORT).show();
+            }
+        });
+        servoright_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(A2_Remote.this, "YOu clicked servoright", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    public void A2_signout(View view) {
+        Intent intent = new Intent(this,A1_Login.class);
+        startActivity(intent);
+
     }
 }
--- Bot_Controller/app/src/main/java/com/example/shaurya/bot_controller/A3_BluetoothList.java
+++ Bot_Controller/app/src/main/java/com/example/shaurya/bot_controller/A3_BluetoothList.java
@@ -1,7 +1,16 @@
 package com.example.shaurya.bot_controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class A3_BluetoothList extends AppCompatActivity {
 
@@ -9,5 +18,26 @@ public class A3_BluetoothList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a3__bluetooth_list);
+        final ArrayList<String> bluetoothlist = new ArrayList<>();
+        bluetoothlist.add("a");bluetoothlist.add("b");bluetoothlist.add("c");bluetoothlist.add("d");bluetoothlist.add("e");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,bluetoothlist);
+
+
+        ListView bluetoothlistview = (ListView) findViewById(R.id.A3_lst_bluetooth);
+       bluetoothlistview.setAdapter(arrayAdapter);
+       bluetoothlistview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+               Toast.makeText(A3_BluetoothList.this,"Connecting to "+bluetoothlist.get(i)+"..",Toast.LENGTH_SHORT).show();
+               connectodevice(i);
+               Intent intent = new Intent(A3_BluetoothList.this,A2_Remote.class);
+               startActivity(intent);
+
+           }
+       });
+
+    }
+
+    private void connectodevice(int i) {
     }
 }
