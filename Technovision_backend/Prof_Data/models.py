332fa3cb5f3d4cd1136a1d20dac3215c56785243
Shubham5798 <shubhamvarankar05@gmail.com>
2018-06-17 14:15:53 +0530

    Prof_Id added

--- Technovision_backend/Prof_Data/models.py
+++ Technovision_backend/Prof_Data/models.py
@@ -7,7 +7,8 @@ class Prof(models.Model):
     Prof_Name = models.CharField(max_length=50)
     Prof_Course = models.ForeignKey(Course, on_delete=models.CASCADE)
     Password = models.CharField(max_length=20)
+    Prof_Id = models.CharField(max_length=10, primary_key=True, default='0000')
 
     def __str__(self):
-        return self.Prof_Name + ' - ' + self.Prof_Course.Course_Code
+        return self.Prof_Id + ' - ' + self.Prof_Name + ' - ' + self.Prof_Course.Course_Code
     
