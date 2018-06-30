b8e79a0b4ddbc9874ebcc7145486931b18e64085
Tezan Sahu <tezansahu@gmail.com>
2018-06-30 17:07:58 +0530

    test_recog code added

---
+++ Python Codes/test_recog.py
@@ -0,0 +1,34 @@
+import technovision_utils as utils
+
+fin=open("ip_address.txt","r")
+url=fin.read()
+fin.close()
+
+video_capture=utils.fetchLiveVideo(url)
+
+
+course="CS101"
+dates_list, roll_nos, attendance_list, known_face_encodings=utils.getDataFromBackend(course)
+print(roll_nos)
+print(len(known_face_encodings))
+studPresent=[]
+
+face_locations = []
+face_encodings = []
+face_rollNos = []
+process_this_frame = True
+
+frame_count=0;
+
+while True:
+	frame_count+=1
+	face_locations, face_encodings, face_rollNos, studPresent=utils.recognizeFaces(video_capture, process_this_frame, roll_nos, known_face_encodings, face_locations, face_encodings, face_rollNos, studPresent)
+	if frame_count%20==0:
+		print("Frame: "+str(frame_count))
+		print(studPresent)
+
+	if utils.checkQuit():
+		break
+
+# Release handle to the webcam
+utils.closeWindows(video_capture)
