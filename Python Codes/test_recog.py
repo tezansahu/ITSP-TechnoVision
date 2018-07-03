a3b177aad0fa2497d24a98911bac38ad3712fda8
Tezan Sahu <tezansahu@gmail.com>
2018-07-03 16:16:06 +0530

    added code to mark attendance in batches

--- Python Codes/test_recog.py
+++ Python Codes/test_recog.py
@@ -18,15 +18,24 @@ face_rollNos = []
 process_this_frame = True
 
 frame_count=0;
-
+tot_stud_present=[]
 while True:
 	frame_count+=1
 	face_locations, face_encodings, face_rollNos, studPresent=utils.recognizeFaces(video_capture, process_this_frame, roll_nos, known_face_encodings, face_locations, face_encodings, face_rollNos, studPresent)
-	if frame_count%20==0:
+	if frame_count%30==0:
 		print("Frame: "+str(frame_count))
-		print(studPresent)
+		for rno in studPresent:
+			stud_id=roll_nos.index(rno)+1
+			utils.markPresent(course, stud_id, attendance_list[stud_id-1])
+		tot_stud_present.extend(studPresent)
+		studPresent=[]
+		print(tot_stud_present)
 
 	if utils.checkQuit():
+		rem_rnos=list(set(roll_nos)-set(tot_stud_present))
+		for rno in rem_rnos:
+			stud_id=roll_nos.index(rno)+1
+			utils.markAbsent(course, stud_id, attendance_list[stud_id-1])
 		break
 
 # Release handle to the webcam
